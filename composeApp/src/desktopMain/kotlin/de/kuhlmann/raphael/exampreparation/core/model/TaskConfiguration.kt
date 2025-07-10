5b8af82bd97fee8210c5e503f7e389de895f0aca
Raphael Kuhlmann <>
2025-07-10 07:54:44 +0200

    Added tasks for enumerations and inheritance

--- composeApp/src/desktopMain/kotlin/de/kuhlmann/raphael/exampreparation/core/model/TaskConfiguration.kt
+++ composeApp/src/desktopMain/kotlin/de/kuhlmann/raphael/exampreparation/core/model/TaskConfiguration.kt
@@ -698,7 +698,205 @@ object TaskConfiguration {
                 TaskElement.Text(value = Res.string.arrays_task_9_answer_3),
                 TaskElement.Text(value = Res.string.arrays_task_9_answer_4)
             )
-        )
+        ),
+        // endregion
+
+        // region enumerations
+        TaskDefinition.SingleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.enumerations_task_1_description),
+            correctAnswer = TaskElement.Text(value = Res.string.enumerations_task_1_answer_1),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.enumerations_task_1_answer_2),
+                TaskElement.Text(value = Res.string.enumerations_task_1_answer_3),
+                TaskElement.Text(value = Res.string.enumerations_task_1_answer_4)
+            )
+        ),
+        TaskDefinition.SingleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.enumerations_task_2_description),
+            correctAnswer = TaskElement.Text(value = Res.string.enumerations_task_2_answer_1),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.enumerations_task_2_answer_2),
+                TaskElement.Text(value = Res.string.enumerations_task_2_answer_3),
+                TaskElement.Text(value = Res.string.enumerations_task_2_answer_4)
+            )
+        ),
+        TaskDefinition.SingleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.enumerations_task_3_description),
+            correctAnswer = TaskElement.Text(value = Res.string.enumerations_task_3_answer_1),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.enumerations_task_3_answer_2),
+                TaskElement.Text(value = Res.string.enumerations_task_3_answer_3),
+                TaskElement.Text(value = Res.string.enumerations_task_3_answer_4)
+            )
+        ),
+        TaskDefinition.MultipleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.enumerations_task_4_description),
+            correctAnswers = listOf(
+                TaskElement.Text(value = Res.string.enumerations_task_4_answer_1),
+                TaskElement.Text(value = Res.string.enumerations_task_4_answer_2)
+            ),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.enumerations_task_4_answer_3),
+                TaskElement.Text(value = Res.string.enumerations_task_4_answer_4)
+            )
+        ),
+        TaskDefinition.MultipleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.enumerations_task_5_description),
+            correctAnswers = listOf(
+                TaskElement.Text(value = Res.string.enumerations_task_5_answer_1),
+                TaskElement.Text(value = Res.string.enumerations_task_5_answer_2)
+            ),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.enumerations_task_5_answer_3),
+                TaskElement.Text(value = Res.string.enumerations_task_5_answer_4)
+            )
+        ),
+        TaskDefinition.SingleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.enumerations_task_6_description),
+            correctAnswer = TaskElement.Text(value = Res.string.enumerations_task_6_answer_1),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.enumerations_task_6_answer_2),
+                TaskElement.Text(value = Res.string.enumerations_task_6_answer_3),
+                TaskElement.Text(value = Res.string.enumerations_task_6_answer_4)
+            )
+        ),
+        TaskDefinition.MultipleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.enumerations_task_7_description),
+            correctAnswers = listOf(
+                TaskElement.Text(value = Res.string.enumerations_task_7_answer_1),
+                TaskElement.Text(value = Res.string.enumerations_task_7_answer_2)
+            ),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.enumerations_task_7_answer_3),
+                TaskElement.Text(value = Res.string.enumerations_task_7_answer_4)
+            )
+        ),
+        TaskDefinition.MultipleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.enumerations_task_8_description),
+            correctAnswers = listOf(
+                TaskElement.Text(value = Res.string.enumerations_task_8_answer_1),
+                TaskElement.Text(value = Res.string.enumerations_task_8_answer_2)
+            ),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.enumerations_task_8_answer_3),
+                TaskElement.Text(value = Res.string.enumerations_task_8_answer_4)
+            )
+        ),
+        TaskDefinition.SingleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.enumerations_task_9_description),
+            correctAnswer = TaskElement.Text(value = Res.string.enumerations_task_9_answer_1),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.enumerations_task_9_answer_2),
+                TaskElement.Text(value = Res.string.enumerations_task_9_answer_3),
+                TaskElement.Text(value = Res.string.enumerations_task_9_answer_4)
+            )
+        ),
+        // endregion
+
+        // region inheritance
+        TaskDefinition.SingleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.inheritance_task_1_description),
+            correctAnswer = TaskElement.Text(value = Res.string.inheritance_task_1_answer_1),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.inheritance_task_1_answer_2),
+                TaskElement.Text(value = Res.string.inheritance_task_1_answer_3),
+                TaskElement.Text(value = Res.string.inheritance_task_1_answer_4)
+            )
+        ),
+        TaskDefinition.SingleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.inheritance_task_2_description),
+            correctAnswer = TaskElement.Text(value = Res.string.inheritance_task_2_answer_1),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.inheritance_task_2_answer_2),
+                TaskElement.Text(value = Res.string.inheritance_task_2_answer_3),
+                TaskElement.Text(value = Res.string.inheritance_task_2_answer_4)
+            )
+        ),
+        TaskDefinition.SingleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.inheritance_task_3_description),
+            correctAnswer = TaskElement.Text(value = Res.string.inheritance_task_3_answer_1),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.inheritance_task_3_answer_2),
+                TaskElement.Text(value = Res.string.inheritance_task_3_answer_3),
+                TaskElement.Text(value = Res.string.inheritance_task_3_answer_4)
+            )
+        ),
+        TaskDefinition.SingleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.inheritance_task_4_description),
+            correctAnswer = TaskElement.Text(value = Res.string.inheritance_task_4_answer_1),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.inheritance_task_4_answer_2),
+                TaskElement.Text(value = Res.string.inheritance_task_4_answer_3),
+                TaskElement.Text(value = Res.string.inheritance_task_4_answer_4)
+            )
+        ),
+        TaskDefinition.MultipleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.inheritance_task_5_description),
+            correctAnswers = listOf(
+                TaskElement.Text(value = Res.string.inheritance_task_5_answer_1),
+                TaskElement.Text(value = Res.string.inheritance_task_5_answer_2)
+            ),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.inheritance_task_5_answer_3),
+                TaskElement.Text(value = Res.string.inheritance_task_5_answer_4)
+            )
+        ),
+        TaskDefinition.SingleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.inheritance_task_6_description),
+            correctAnswer = TaskElement.Text(value = Res.string.inheritance_task_6_answer_1),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.inheritance_task_6_answer_2),
+                TaskElement.Text(value = Res.string.inheritance_task_6_answer_3),
+                TaskElement.Text(value = Res.string.inheritance_task_6_answer_4)
+            )
+        ),
+        TaskDefinition.MultipleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.inheritance_task_7_description),
+            correctAnswers = listOf(
+                TaskElement.Text(value = Res.string.inheritance_task_7_answer_1),
+                TaskElement.Text(value = Res.string.inheritance_task_7_answer_2)
+            ),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.inheritance_task_7_answer_3),
+                TaskElement.Text(value = Res.string.inheritance_task_7_answer_4)
+            )
+        ),
+        TaskDefinition.SingleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.inheritance_task_8_description),
+            correctAnswer = TaskElement.Text(value = Res.string.inheritance_task_8_answer_1),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.inheritance_task_8_answer_2),
+                TaskElement.Text(value = Res.string.inheritance_task_8_answer_3),
+                TaskElement.Text(value = Res.string.inheritance_task_8_answer_4)
+            )
+        ),
+        TaskDefinition.SingleChoice(
+            category = Category.ENUMERATIONS,
+            description = TaskElement.Text(value = Res.string.inheritance_task_9_description),
+            correctAnswer = TaskElement.Text(value = Res.string.inheritance_task_9_answer_1),
+            answers = listOf(
+                TaskElement.Text(value = Res.string.inheritance_task_9_answer_2),
+                TaskElement.Text(value = Res.string.inheritance_task_9_answer_3),
+                TaskElement.Text(value = Res.string.inheritance_task_9_answer_4)
+            )
+        ),
         // endregion
 
     )
